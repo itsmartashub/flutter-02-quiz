9924ee6d150bf81567c182c08a37d815e7ebbbc5
itsmartashub <coderawii@gmail.com>
2023-08-22 11:13:51 +0200

    02-007 add `quiz.dart` & `questions_screen.dart`

---
+++ lib/quiz.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:quiz_app/start_screen.dart';
+
+class Quiz extends StatefulWidget {
+  const Quiz({super.key});
+
+  @override
+  State<Quiz> createState() => _QuizState();
+}
+
+class _QuizState extends State<Quiz> {
+  @override
+  Widget build(context) {
+    return MaterialApp(
+        // Scaffold se ugl koristi da se setuju podesavanja za SCREEN nase app (bg color itd)
+        // Elem, a posto hocemo sad ovde gradient bg, koristicemo Container jr nam on omogucava da setujemo decoration na njega. I wrapovacemo ovaj StartScreen widget u njega jer zelimo da koristimo isti setup, isti screen bg color na svim screens koje cemo dodati. Zato ne stavljamo ovaj gradient na specific screen (kao recimo StartScreen), vec cemo StartScreen da wrapujemo
+        // home: Scaffold(body: StartScreen())));
+        home: Scaffold(
+            body: Container(
+                decoration: const BoxDecoration(
+                    gradient: LinearGradient(colors: [
+                  Color.fromARGB(255, 93, 185, 110),
+                  Color.fromARGB(255, 116, 106, 211)
+                ], begin: Alignment.topLeft, end: Alignment.bottomRight)),
+                child: const StartScreen())));
+  }
+}
