ce8128d32a52264c3e477158e55f1c88cd2928e7
itsmartashub <coderawii@gmail.com>
2023-08-22 16:00:05 +0200

    03-022 `shuffle()`. mutating values in memory

--- lib/models/quiz_question.dart
+++ lib/models/quiz_question.dart
@@ -3,4 +3,13 @@ class QuizQuestion {
 
   final String text;
   final List<String> answers;
+
+/*
+ali pre engo sto cemo odraditi shuffle() zelimo prvo da kopiramo originalu Listu odgovora, jer nam se u njoj uvek tacan odgovor nalazi na prvom mestu, a shuffle metod mutira originalnu listu, za razliku od map() metoda koji ne mutira originalnu listu vec kreira svoju listu, dakle ne bismo znali koji je tacan odg.  */
+  List<String> getShuffledAnswers() {
+    //! kreiranje kopirane originalne liste, to radimo sa List.of(originalna_lista). To je built-in List classa, dakle List nije samo type vec i class built into Dart. .of je constructor fn List klase. of kreiranja kopiju originalne liste
+    final shuffledList = List.of(answers);
+    shuffledList.shuffle();
+    return shuffledList;
+  }
 }
