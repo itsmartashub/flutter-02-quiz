4d2c7045f14885e4a76460f22ab47346b052edd1
itsmartashub <coderawii@gmail.com>
2023-08-22 14:15:43 +0200

    003-017 configuring `Column()` inside `SizedBox()`
    - `width: double.infinity`  ▶️  za full width, tj take as much space is avaible
    
    -  mainAxisAlignment: MainAxisAlignment.center` ▶️ novi nacin centriranja kontenta. Ima vrednosti kao flexbox u css, `.spaceBetween`, `.spaceAround`, `.spaceEvenly`. Ako se koristi u `Column()` on vertikalno centrira jer su u `Column()`-u sadrzaj jedan ispod drugog, a ako se koristi u `Row()` onda horizontalno centrira jer je tu sadrzaj jedan pored drugog, dakle u zavisnosti od MAIN AXIS-a se vrsi centriranje.

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -10,6 +10,30 @@ class QuestionsScreen extends StatefulWidget {
 class _QuestionsScreenState extends State<QuestionsScreen> {
   @override
   Widget build(context) {
-    return const Text('_QuestionsScreenState');
+    /* dodajemo SizedBox da bi nas Column tj content zauzimao citavu sirinu ekrana koliko god ona bila, to cemo odraditi sa double.infinity. To je special value koji znaci: use as much width as you can, as wide is possible */
+    return SizedBox(
+      width: double.infinity,
+      child: Column(
+        /* novi nacin za centriranje kontenta. Ako se koristi u Column on vertikalno centrira jer su u Columnu sadrzaj jedan ispod drugog, a ako se koristi u Row() onda horizontalno centrira jer je tu sadrzaj jedan pored drugog, dakle u zavisnosti od MAIN AXIC-a se vrsi centriranje.
+        MainAxisAlignment ima vrednosti kao flexbox u css, .spaceBetween, .spaceAround, .spaceEvenly */
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text('The question...'),
+          const SizedBox(height: 20),
+          ElevatedButton(
+            onPressed: () {},
+            child: const Text('Answer 1'),
+          ),
+          ElevatedButton(
+            onPressed: () {},
+            child: const Text('Answer 2'),
+          ),
+          ElevatedButton(
+            onPressed: () {},
+            child: const Text('Answer 3'),
+          ),
+        ],
+      ),
+    );
   }
 }
