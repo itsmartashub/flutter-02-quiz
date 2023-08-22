8ac0b66ad7b64178562cf76facb946aefaacf586
itsmartashub <coderawii@gmail.com>
2023-08-22 15:43:14 +0200

    03-021 `crossAxisAlignment`, `margin`, `textAlign`

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -18,44 +18,49 @@ class _QuestionsScreenState extends State<QuestionsScreen> {
     /* dodajemo SizedBox da bi nas Column tj content zauzimao citavu sirinu ekrana koliko god ona bila, to cemo odraditi sa double.infinity. To je special value koji znaci: use as much width as you can, as wide is possible */
     return SizedBox(
       width: double.infinity,
-      child: Column(
-        /* novi nacin za centriranje kontenta. Ako se koristi u Column on vertikalno centrira jer su u Columnu sadrzaj jedan ispod drugog, a ako se koristi u Row() onda horizontalno centrira jer je tu sadrzaj jedan pored drugog, dakle u zavisnosti od MAIN AXIS-a se vrsi centriranje.
-        * - MainAxisAlignment ima vrednosti kao flexbox u css, .spaceBetween, .spaceAround, .spaceEvenly */
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(
-            currentQuestion.text,
-            style: const TextStyle(color: Colors.white, fontSize: 22),
-          ),
-          const SizedBox(height: 20),
-          /* ElevatedButton(
-            onPressed: () {},
-            child: const Text('Answer 1'),
-          ),
-          ElevatedButton(
-            onPressed: () {},
-            child: const Text('Answer 2'),
-          ),
-          ElevatedButton(
-            onPressed: () {},
-            child: const Text('Answer 3'),
-          ), */
-          //? sa position argument; order vazan
-          // AnswerButton('Answer text', () {}),
-          //? sa named argument; order nevazan
-          /* AnswerButton(onTap: () {}, answerText: currentQuestion.answers[0]),
-          AnswerButton(onTap: () {}, answerText: currentQuestion.answers[1]),
-          AnswerButton(onTap: () {}, answerText: currentQuestion.answers[2]), */
+      child: Container(
+        margin: const EdgeInsets.all(40),
+        child: Column(
+          /* novi nacin za centriranje kontenta. Ako se koristi u Column on vertikalno centrira jer su u Columnu sadrzaj jedan ispod drugog, a ako se koristi u Row() onda horizontalno centrira jer je tu sadrzaj jedan pored drugog, dakle u zavisnosti od MAIN AXIS-a se vrsi centriranje.
+          * - MainAxisAlignment ima vrednosti kao flexbox u css, .spaceBetween, .spaceAround, .spaceEvenly */
+          mainAxisAlignment: MainAxisAlignment.center,
 
-          /* ! ovo je podvuceno sve crvenim jer children: zeli Listu widgeta, a mi ovim map kreiramo novu Listu, Listu unutar Liste. zato moramo korisiti spread operator ...
-          ! Dakle ono sto spreading radi je pretvara neted listu (listu unutar liste) u jednu listu. NPR: [[1,2,3],4] u [1,2,3,4] */
-          ...currentQuestion.answers.map((answer) {
-            return AnswerButton(
-              onTap: () {},
-              answerText: answer,
-            );
-          })
-        ],
+          /* 
+          - sa crossAxisAlignment: ce se itemi rasiriti koliko god je to moguce,da filluju citavu sirinu kolone. Ali sad vidimo da uopste nemamo levo i desno prostora od ivica, sto takodje ne zelimo 
+          - Zato cemo koristiti margin, tj idemo desni klik na Column, pa Refactor... pa Wrap with Container jer Container widget ima margin argument koji mozemo setovati
+          -Takodje zelimo da centriramo i Text (pitanje), zato idemo u Text() widget, na sam Text(), dakle ne u style, dodajemo textAlign: TextAlign.center */
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              currentQuestion.text,
+              style: const TextStyle(color: Colors.white, fontSize: 22),
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(height: 20),
+            /* ElevatedButton(
+              onPressed: () {},
+              child: const Text('Answer 1'),
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              child: const Text('Answer 2'),
+            ),*/
+            //? sa position argument; order vazan
+            // AnswerButton('Answer text', () {}),
+            //? sa named argument; order nevazan
+            /* AnswerButton(onTap: () {}, answerText: currentQuestion.answers[0]),
+            AnswerButton(onTap: () {}, answerText: currentQuestion.answers[1]),*/
+
+            /* ! ovo je podvuceno sve crvenim jer children: zeli Listu widgeta, a mi ovim map kreiramo novu Listu, Listu unutar Liste. zato moramo korisiti spread operator ...
+            ! Dakle ono sto spreading radi je pretvara neted listu (listu unutar liste) u jednu listu. NPR: [[1,2,3],4] u [1,2,3,4] */
+            ...currentQuestion.answers.map((answer) {
+              return AnswerButton(
+                onTap: () {},
+                answerText: answer,
+              );
+            })
+          ],
+        ),
       ),
     );
   }
