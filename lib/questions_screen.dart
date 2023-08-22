941e4c4fe7392251dfa63f02ef70ce4897856a49
itsmartashub <coderawii@gmail.com>
2023-08-22 16:08:44 +0200

    03-023 managing questions index state

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -11,9 +11,21 @@ class QuestionsScreen extends StatefulWidget {
 }
 
 class _QuestionsScreenState extends State<QuestionsScreen> {
+  // inicijalno treba da je 0, ali treba da se promeni svaki x kada se klikne odgovor na neko pitanje
+  var currentQuestionIndex = 0;
+
+  void answerQuestion() {
+    setState(() {
+      // currentQuestionIndex = currentQuestionIndex + 1;
+      // currentQuestionIndex += 1;
+      currentQuestionIndex++; // increments the value by 1
+    });
+  }
+
   @override
   Widget build(context) {
-    final currentQuestion = questions[0];
+    // final currentQuestion = questions[0];
+    final currentQuestion = questions[currentQuestionIndex];
 
     /* dodajemo SizedBox da bi nas Column tj content zauzimao citavu sirinu ekrana koliko god ona bila, to cemo odraditi sa double.infinity. To je special value koji znaci: use as much width as you can, as wide is possible */
     return SizedBox(
@@ -56,7 +68,7 @@ class _QuestionsScreenState extends State<QuestionsScreen> {
             // ...currentQuestion.answers.map((answer) {
             ...currentQuestion.getShuffledAnswers().map((answer) {
               return AnswerButton(
-                onTap: () {},
+                onTap: answerQuestion,
                 answerText: answer,
               );
             })
