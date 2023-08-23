721eb065189175050df6c54e3bb21ff3b4845ea6
itsmartashub <coderawii@gmail.com>
2023-08-23 15:00:24 +0200

    Revert "accessing `Map` values & using type casting. `List<Map<String, Object>>`"
    This reverts commit c6f5dbe5c38f4e5a9f3219f128be0b5b7bcf0e12.

--- lib/results_screen.dart
+++ lib/results_screen.dart
@@ -1,32 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:quiz_app/data/questions.dart';
 
 class ResultsScreen extends StatelessWidget {
   const ResultsScreen({super.key, required this.chosenAnswers});
 
   final List<String> chosenAnswers;
-
-//? Map<K, V>   |   Map Key Value
-  /* ! Map je, kao i List, generic type, gde specifiramo value naseg Key, i type naseg Value. Dakle imamo jos jedan <>, takoreci nested angulars. Gde cemo za Key da stavimo String, ali za Values cemo imati razlicite types podataka, i Stringove i Numbers, zato najbolje napisati Object, jer su sve vrednosti u Dartu Object, to mu dodje flexible type koji dozvoljava razlicite vvrste vrednosti
-
-  Umesto ovog, alternativno bismo mogli da kreiramo novu klasu koje bi definsiala kako nas SummaryObject treba da izgleda, i onda bismo mogli da instanciramo nekoliko SummaryObjecta-a ovde (ja to kapiram kao componente sto su u Vue). I to je takodje dobro resenje, ali Max nam je hteo pokazati Map<K,V> i zato radi na ovaj nacin.
-  
-  Dakle getSummaryData treba da vrati Listu Map-a, */
-  List<Map<String, Object>> getSummaryData() {
-    final List<Map<String, Object>> summary = [];
-
-    for (var i = 0; i < chosenAnswers.length; i++) {
-      summary.add({
-        // Map se sastoji od Key-Value pairs
-        'question_index': i,
-        'question': questions[i].text,
-        'correct_answer': questions[i].answers[0],
-        'user_answer': chosenAnswers[i]
-      });
-    }
-
-    return summary;
-  }
 
   @override
   Widget build(BuildContext context) {
